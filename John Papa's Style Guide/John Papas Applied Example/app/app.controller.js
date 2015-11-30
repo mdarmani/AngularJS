d627c945b82ef237102d2604f935e63efe13b5b7
unknown <theassociator@gmail.com>
2015-11-30 18:12:49 +0100

    More updates

--- John Papa's Style Guide/John Papas Applied Example/app/app.controller.js
+++ John Papa's Style Guide/John Papas Applied Example/app/app.controller.js
@@ -6,18 +6,24 @@
         .module('myApp')
 
         /* Creating a controller using the Controller As method */
-        .controller('controllerAsMethod', controllerAsMethod);
+        .controller('controllers', controllers)
 
-		function controllerAsMethod($scope, $log) {
+		function controllers($scope, $log) {
+
 			//Set this to vm to represent ViewModel and also prevent jshint warnings by including the comment below.
-
 			/* jshint validthis: true */
     		var vm = this;
 
     		//Adding the various descriptions
     		vm.array4 = [
-    			'4.1 Using the controller as Syntax.',
-    			'4.2 '
+    			'4.1 controllerAs View Syntax',
+    			'4.2 controllerAs Controller Syntax',
+    			'4.3 controllerAs with vm',
+    			'4.4 Bindable Members Up Top',
+    			'4.4 Function Declarations to Hide Implementation Details',
+    			'4.5 Defer Controller Logic to Services',
+    			'4.6 Keep Controllers Focused',
+    			'4.7 Assigning Controllers'
     		]
 
     		//Create an empty array
@@ -51,6 +57,27 @@
 				vm.count += 1;
 			}
 
-		}
+		
 
+			/* 4.4 Bindable Members Up Top */
+			vm.gotoSession = gotoSession;
+			vm.refresh = refresh;
+			vm.search = search;
+			vm.sessions = [];
+			vm.title = 'Sessions';
+
+			////////////
+
+			function gotoSession() {
+			 	$log.info('Executed gotoSession()');
+			}
+
+			function refresh() {
+			   	$log.info('Executed refresh()');		    
+			}
+
+			function search() {
+			   	$log.info('Executed search()');
+			}
+		}
 })();
