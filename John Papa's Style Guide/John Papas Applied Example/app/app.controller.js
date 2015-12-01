b14a685cf2d4039a79df494c5fae2e7b8ae8a025
unknown <theassociator@gmail.com>
2015-12-01 17:56:13 +0100

    Another update

--- John Papa's Style Guide/John Papas Applied Example/app/app.controller.js
+++ John Papa's Style Guide/John Papas Applied Example/app/app.controller.js
@@ -8,23 +8,13 @@
         /* Creating a controller using the Controller As method */
         .controller('controllers', controllers)
 
+        .controller('SessionsController', SessionsController)
+
 		function controllers($scope, $log) {
 
 			//Set this to vm to represent ViewModel and also prevent jshint warnings by including the comment below.
 			/* jshint validthis: true */
     		var vm = this;
-
-    		//Adding the various descriptions
-    		vm.array4 = [
-    			'4.1 controllerAs View Syntax',
-    			'4.2 controllerAs Controller Syntax',
-    			'4.3 controllerAs with vm',
-    			'4.4 Bindable Members Up Top',
-    			'4.4 Function Declarations to Hide Implementation Details',
-    			'4.5 Defer Controller Logic to Services',
-    			'4.6 Keep Controllers Focused',
-    			'4.7 Assigning Controllers'
-    		]
 
     		//Create an empty array
 			vm.original = [''];
@@ -78,19 +68,25 @@
             );		
 
 			/* 4.4 Bindable Members Up Top */
+			
+		}
+
+
+
+		function SessionsController($log, sessionDataService) {
+			var vm = this;
 			vm.gotoSession = gotoSession;
-			vm.refresh = refresh;
+			vm.refresh = sessionDataService.refresh;
 			vm.search = search;
 			vm.sessions = [];
-			vm.title = 'Sessions';
-
-			////////////
+    		vm.title = 'Sessions';
 
 			function gotoSession() {
 			 	$log.info('Executed gotoSession()');
 			}
 
 			function refresh() {
+				//sessions.push('2erferik');
 			   	$log.info('Executed refresh()');		    
 			}
 
--- John Papa's Style Guide/John Papas Applied Example/app/app.factory.js
+++ John Papa's Style Guide/John Papas Applied Example/app/app.factory.js
@@ -4,4 +4,13 @@
 
     angular
         .module('myApp')
+
+        .factory('sessionDataService', function ($log) {
+        	$log.info('not done...');
+        	return function refresh() {
+				//sessions.push('2erferik');
+			   	$log.info('Executed refresh()');		    
+			}
+        	$log.info('done!');	  
+		})
 })();
