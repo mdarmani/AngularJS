4f3aa6374f25d0a768afe4277fd9cecb5f35eb90
unknown <theassociator@gmail.com>
2015-12-10 11:01:32 +0100

    first commit 10-12-15

--- John Papa's Style Guide/John Papas Applied Example/app/app.config.js
+++ John Papa's Style Guide/John Papas Applied Example/app/app.config.js
@@ -4,8 +4,28 @@
 
     angular
     	.module('myApp')
+
+    	.config(config);
+
+    	function config($routeProvider) {
+		    $routeProvider
+		        .when('/avengers', {
+		            templateUrl: 'avengers.html',
+		            controller: 'Avengers',
+		            controllerAs: 'vm'
+		        }).when('/avengers', {
+		            templateUrl: 'avengers.html',
+		            controller: 'Avengers',
+		            controllerAs: 'vm'
+		        }).when('/avengers', {
+		            templateUrl: 'avengers.html',
+		            controller: 'Avengers',
+		            controllerAs: 'vm'
+		        }).when('/avengers', {
+		            templateUrl: 'avengers.html',
+		            controller: 'Avengers',
+		            controllerAs: 'vm'
+		        }).otherwise({redirectTo: '/views/view1'});
+		}
     	
-    	.config(['$routeProvider',  function($routeProvider) {
-		  $routeProvider.otherwise({redirectTo: '/view1'});
-		}])  
 })();
